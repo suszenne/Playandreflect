cd7a620c7b4801e56e6f580c593e6bee06b3d254
suszenne <37084433+suszenne@users.noreply.github.com>
2022-06-06 19:07:14 +0200

    Add files via upload

---
+++ sketch18.js
@@ -0,0 +1,14 @@
+let im;
+let em;
+
+
+function preload(){
+ im = loadImage("Images/Why.png")
+em = loadImage("Images/vinili.jpg")
+}
+
+function setup() {
+    createCanvas(1144, 536);
+    background(em);
+    noStroke();
+}
